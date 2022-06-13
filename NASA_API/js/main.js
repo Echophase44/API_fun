fb05cb2070eb6431d14c869fda242488782e24ae
Nick <Echophase44@github.com>
2022-06-13 16:37:14 -0400

    Added Nasa api fetch info

--- NASA_API/js/main.js
+++ NASA_API/js/main.js
@@ -1,1 +1,12 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+//6bRJ5VcSUKLRydyFKUctSYZk4uQpks7rSvzrYh5v
+fetch(
+  `https://api.nasa.gov/planetary/apod?api_key=6bRJ5VcSUKLRydyFKUctSYZk4uQpks7rSvzrYh5v`
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
