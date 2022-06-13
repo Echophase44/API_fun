a2a8a2fb3ac12e04cae6fefac7ba7b436e779c5c
Nick <Echophase44@github.com>
2022-06-13 10:44:22 -0400

    Added foward and backward functionality

--- CocktailAPI/js/main.js
+++ CocktailAPI/js/main.js
@@ -1,4 +1,8 @@
 document.querySelector("button").addEventListener("click", getDrinks);
+document.querySelector(".forward").addEventListener("click", goForward);
+document.querySelector(".backward").addEventListener("click", goBackward);
+let currentData = {};
+let currentSelection = 0;
 
 function getDrinks() {
   let drinkType = document.querySelector("input").value;
@@ -6,7 +10,9 @@ function getDrinks() {
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkType}`)
     .then((res) => res.json())
     .then((data) => {
+      currentSelection = 0;
       console.log(data);
+      currentData = data;
       document.querySelector("img").src = data.drinks[0].strDrinkThumb;
       document.querySelector("h2").innerText = data.drinks[0].strDrink;
       document.querySelector("h3").innerText = data.drinks[0].strInstructions;
@@ -14,4 +20,26 @@ function getDrinks() {
     .catch((err) => {
       console.log(`error ${err}`);
     });
+}
+
+function goForward() {
+  currentSelection = currentSelection + 1;
+
+  document.querySelector("img").src =
+    currentData.drinks[currentSelection].strDrinkThumb;
+  document.querySelector("h2").innerText =
+    currentData.drinks[currentSelection].strDrink;
+  document.querySelector("h3").innerText =
+    currentData.drinks[currentSelection].strInstructions;
+}
+
+function goBackward() {
+  currentSelection = currentSelection - 1;
+
+  document.querySelector("img").src =
+    currentData.drinks[currentSelection].strDrinkThumb;
+  document.querySelector("h2").innerText =
+    currentData.drinks[currentSelection].strDrink;
+  document.querySelector("h3").innerText =
+    currentData.drinks[currentSelection].strInstructions;
 }
