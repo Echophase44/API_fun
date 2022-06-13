201f974454d50345fdb5638b781bcf7d14d613ef
Nick <Echophase44@github.com>
2022-06-13 06:40:57 -0400

    Button and image now working/uploading to index

--- CocktailAPI/js/main.js
+++ CocktailAPI/js/main.js
@@ -1,9 +1,13 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(`error ${err}`);
-  });
+document.querySelector("button").addEventListener("click", getDrinks);
+
+function getDrinks() {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      document.querySelector("img").src = data.drinks[0].strDrinkThumb;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
