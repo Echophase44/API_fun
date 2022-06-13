a4ca7afa4b053bfe03868f19dc363d076456af79
Nick <Echophase44@github.com>
2022-06-13 09:26:09 -0400

    Added input functionality

--- CocktailAPI/js/main.js
+++ CocktailAPI/js/main.js
@@ -1,11 +1,15 @@
 document.querySelector("button").addEventListener("click", getDrinks);
 
 function getDrinks() {
-  fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+  let drinkType = document.querySelector("input").value;
+
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkType}`)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
       document.querySelector("img").src = data.drinks[0].strDrinkThumb;
+      document.querySelector("h2").innerText = data.drinks[0].strDrink;
+      document.querySelector("h3").innerText = data.drinks[0].strInstructions;
     })
     .catch((err) => {
       console.log(`error ${err}`);
