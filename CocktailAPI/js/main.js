58d73adb1842fe594f818b46db92b898b22558db
Nick <Echophase44@github.com>
2022-06-13 21:35:40 -0400

    Added edge cases and cleaned up redundant code

--- CocktailAPI/js/main.js
+++ CocktailAPI/js/main.js
@@ -5,7 +5,7 @@ let currentData = {};
 let currentSelection = 0;
 
 function getDrinks() {
-  let drinkType = document.querySelector("input").value;
+  let drinkType = document.querySelector("input").value.trim();
   drinkType = drinkType.replace(" ", "_");
 
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkType}`)
@@ -24,19 +24,22 @@ function getDrinks() {
 }
 
 function goForward() {
-  currentSelection = currentSelection + 1;
-
-  document.querySelector("img").src =
-    currentData.drinks[currentSelection].strDrinkThumb;
-  document.querySelector("h2").innerText =
-    currentData.drinks[currentSelection].strDrink;
-  document.querySelector("h3").innerText =
-    currentData.drinks[currentSelection].strInstructions;
+  if (currentSelection === currentData.drinks.length - 1) {
+  } else {
+    currentSelection = currentSelection + 1;
+    updateDisplay();
+  }
 }
 
 function goBackward() {
-  currentSelection = currentSelection - 1;
+  if (currentSelection === 0) {
+  } else {
+    currentSelection = currentSelection - 1;
+    updateDisplay();
+  }
+}
 
+function updateDisplay() {
   document.querySelector("img").src =
     currentData.drinks[currentSelection].strDrinkThumb;
   document.querySelector("h2").innerText =
