102598d2184e8a766579000b845fc4e94ebc456e
Nick <Echophase44@github.com>
2022-06-13 21:43:24 -0400

    Nasa api working to update images

--- NASA_API/js/main.js
+++ NASA_API/js/main.js
@@ -1,11 +1,12 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
 //6bRJ5VcSUKLRydyFKUctSYZk4uQpks7rSvzrYh5v
 fetch(
-  `https://api.nasa.gov/planetary/apod?api_key=6bRJ5VcSUKLRydyFKUctSYZk4uQpks7rSvzrYh5v`
+  `https://api.nasa.gov/planetary/apod?api_key=6bRJ5VcSUKLRydyFKUctSYZk4uQpks7rSvzrYh5v&date=2001-07-16`
 )
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
+    document.querySelector("img").src = data.hdurl;
   })
   .catch((err) => {
     console.log(`error ${err}`);
