7692a53c9ec283b8b6e0fce52b8658bfa3f12227
Nick <Echophase44@github.com>
2022-06-14 22:07:20 -0400

    Working api, images, and play round feature

--- Card-Game/js/main.js
+++ Card-Game/js/main.js
@@ -1,15 +1,32 @@
-document.querySelector("button").addEventListener("click", getFetch);
+document.querySelector("#newDeck").addEventListener("click", getDeck);
+document.querySelector("#playRound").addEventListener("click", playMatch);
 
-function getFetch() {
-  const choice = document.querySelector("input").value;
+currentDeckId = "";
+
+function getDeck() {
   const url = "https://deckofcardsapi.com/api/deck/new/";
 
   fetch(url)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data);
+      currentDeckId = data.deck_id;
     })
     .catch((err) => {
       console.log(`error ${err}`);
     });
+}
+
+function playMatch() {
+  fetch(`https://deckofcardsapi.com/api/deck/${currentDeckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      document.querySelector("#player1").src = data.cards[0].image;
+      document.querySelector("#player2").src = data.cards[1].image;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+  //
 }
